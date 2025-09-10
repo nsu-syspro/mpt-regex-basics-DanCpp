da06186ed47ac16aec630e2cf7201072ce4cb5ff
DanCpp <danila.ushakov.08@mail.ru>
2025-09-10 10:59:46 +0700

    upd task5

--- solution/task5.sh
+++ solution/task5.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sed -E 's/(\w*)\s(\w*)\s{0,1}(\w*){0,1},([0-9]*)/\2 \1 (\4)/' ./data/students.csv > ./data/students.txt
+sed -E "s/(\w*[-']*\w*)\s(\w*)\s{0,1}(\w*){0,1},([0-9]*)/\2 \1 (\4)/" ./data/students.csv > ./data/students.txt
+
